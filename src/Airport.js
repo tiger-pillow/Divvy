93b4ab563d4fb6ee8ec2e826d43632be28fecc3f
Yiyuan Fang <tigerpillow@Yiyuans-MacBook-Pro.local>
2020-06-09 06:47:35 -0400

    added date selector

--- src/Airport.js
+++ src/Airport.js
@@ -1,16 +1,24 @@
 import React, {useState} from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import './Airport.css';
 import Basket from  './Basket.js';
 import './Basket.css';
 import {user, airports} from './fakedata.js';
 
 function Airport () {
+	const [startDate, setStartDate] = useState(new Date());
+	
 	const airportlist = airports.map((name, i)=> {
 		return <Block name = {airports[i].name} />
 	})
 
 	return (
 		<div className = "f3 tc">
+			<header>
+			<DatePicker selected = {startDate} onChange= {date => setStartDate(date)}/>
+			<p> {startDate.toString()} </p>
+			</header>
 			{airportlist}
 		</div> 
 		)
